42c842eec642863e7b731cfd6ba058b14cfa8d56
developerwill <info@wilsongomes.me>
2023-10-26 17:06:05 +0000

    Attribute directives: ngStyle

--- src/app/server/server.module.ts
+++ src/app/server/server.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ServerComponent } from './server.component';
+import { SharedModule } from '../Shared.module';
 
 @NgModule({
     declarations: [
         ServerComponent
+    ],
+    imports: [
+        SharedModule
     ],
     exports: [
         ServerComponent
