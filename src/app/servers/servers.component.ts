5521f8accf930a0491683137a990c49d845f7e2d
developerwill <info@wilsongomes.me>
2023-10-26 13:51:56 +0000

    Passing adn using  data with event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
     allowNewServer: boolean = false;
     serverCreationStatus: string = 'No server was created!';
+    serverName: string = '';
 
     constructor() {
         setTimeout(() => {
@@ -23,5 +24,9 @@ export class ServersComponent implements OnInit {
 
     onCreateServer() {
         this.serverCreationStatus = 'Server was created!';
+    }
+
+    onUpdateServerName(event: Event) {
+        this.serverName = (<HTMLInputElement>event.target).value;
     }
 }
